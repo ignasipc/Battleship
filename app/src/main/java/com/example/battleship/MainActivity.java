34253e07d88725130315ff38df79f494be2fec54
ignasipc <ignasipc22@gmail.com>
2026-03-27 11:33:25 +0100

    P0's third point finished (Buttons minimum functionality)

--- app/src/main/java/com/example/battleship/MainActivity.java
+++ app/src/main/java/com/example/battleship/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.battleship;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -13,6 +17,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private enum estatsJoc {JUGANT, ATURADA};
+    private static estatsJoc estatActualJoc = estatsJoc.ATURADA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +36,51 @@ public class MainActivity extends AppCompatActivity {
         TextView messageTextView = (TextView) findViewById(R.id.messagesTextView);
         messageTextView.setMovementMethod(new ScrollingMovementMethod());
 
+        Button botoAturar = (Button) findViewById(R.id.buttonStop);
+        Button botoNouJoc = (Button) findViewById(R.id.buttonNewGame);
+        Button botoConnectar = (Button) findViewById(R.id.buttonConnect);
+        Button botoPista = (Button) findViewById(R.id.buttonHint);
 
+        if (estatActualJoc == estatsJoc.JUGANT){
+            // Aturar i Pista activats, la resta desactivats
+            botoAturar.setEnabled(true);
+            botoPista.setEnabled(true);
+            botoNouJoc.setEnabled(false);
+            botoConnectar.setEnabled(false);
+        } else if (estatActualJoc == estatsJoc.ATURADA){
+            // Nou joc i Connectar activats, la resta desactivats
+            botoNouJoc.setEnabled(true);
+            botoConnectar.setEnabled(true);
+            botoAturar.setEnabled(false);
+            botoPista.setEnabled(false);
+        }
+
+        botoAturar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("Button Pressed", "Button Stop has ben pressed");
+            }
+        });
+
+        botoNouJoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("Button Pressed", "Button New Game has ben pressed");
+            }
+        });
+
+        botoConnectar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("Button Pressed", "Button Connect has ben pressed");
+            }
+        });
+
+        botoPista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("Button Pressed", "Button Hint has ben pressed");
+            }
+        });
     }
 }
