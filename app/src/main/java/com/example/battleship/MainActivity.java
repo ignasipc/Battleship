01e0e3d1cc6727b3e310a77c19448350fa7c1780
ignasipc <ignasipc22@gmail.com>
2026-03-28 10:30:32 +0100

    P0 finished. Ready to test on the emulator

--- app/src/main/java/com/example/battleship/MainActivity.java
+++ app/src/main/java/com/example/battleship/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.battleship;
 
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.pm.ActivityInfo;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,9 +18,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+// TODO: Fer testing de la P0 amem si funciona tot correctament a un emulador o un mòvil.
+
 public class MainActivity extends AppCompatActivity {
     private enum estatsJoc {JUGANT, ATURADA};
     private static estatsJoc estatActualJoc = estatsJoc.ATURADA;
+    private static SurfaceView dibuix;                                                              // TODO: Comprobar más adelante si es correcta esta manera de obtener 'dibuix'
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,16 +36,23 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        // Posam apaisada la pantalla (P0.1)
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
+        // Feim el TextView desplaçable (P0.1.6)
         TextView messageTextView = (TextView) findViewById(R.id.messagesTextView);
         messageTextView.setMovementMethod(new ScrollingMovementMethod());
+
+        dibuix = (SurfaceView) findViewById(R.id.hisSurfaceView);
+
+        dibuix.post(() -> pintar());
 
         Button botoAturar = (Button) findViewById(R.id.buttonStop);
         Button botoNouJoc = (Button) findViewById(R.id.buttonNewGame);
         Button botoConnectar = (Button) findViewById(R.id.buttonConnect);
         Button botoPista = (Button) findViewById(R.id.buttonHint);
 
+        // Lògica de l'estat actual del joc que indica quins botons están activats i quins no
         if (estatActualJoc == estatsJoc.JUGANT){
             // Aturar i Pista activats, la resta desactivats
             botoAturar.setEnabled(true);
@@ -82,5 +94,25 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("Button Pressed", "Button Hint has ben pressed");
             }
         });
+    }
+
+    /**
+     * Mètode per pintar a un SurfaceView anomenat dibuix.
+     */
+    public static void pintar(){
+        if(dibuix.getHolder().getSurface().isValid()){
+            int alt = dibuix.getHeight();
+            int ampla = dibuix.getWidth();
+            Canvas canvas = dibuix.getHolder().lockCanvas();
+            canvas.drawColor(Color.BLACK);
+
+            // El nostre mètode de dibuixat
+            Paint p = new Paint();
+            p.setColor(Color.RED);
+            p.setStrokeWidth(5);
+            canvas.drawLine(0,0, alt, ampla, p);
+
+            dibuix.getHolder().unlockCanvasAndPost(canvas);
+        }
     }
 }
